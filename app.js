0a22ba634196988a231483769495c32bea6d27e2
Rodrigo Cabrera <rodrigocabrera.developer@gmail.com>
2025-03-16 10:23:43 -0300

    Primera tarea concluida. Se inicia el array para almacenar nombres

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// 1. Crear un array para almacenar los nombres
+
+let Amigos = [];
+console.log(Amigos);
+console.log(Amigos.length)
+
+// 2. Implementa una función para agregar amigos
+
+
+// 3. Implementa una función para actualizar la lista de amigos
+
+// 4. Implementa una función para sortear los amigos
